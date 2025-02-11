1199d0d6d6435b141a38a2ccfed198aca29d092f
DeaHyun0911 <eogus0911@gmail.com>
2025-02-11 23:44:18 +0900

    test: review create test
    - 리뷰 생성 통합 테스트

--- src/main/java/com/sparta/levelup_backend/domain/review/controller/ReviewController.java
+++ src/main/java/com/sparta/levelup_backend/domain/review/controller/ReviewController.java
@@ -7,10 +7,10 @@ import com.sparta.levelup_backend.domain.review.dto.request.ReviewRequestDto;
 import com.sparta.levelup_backend.domain.review.dto.response.ReviewResponseDto;
 import com.sparta.levelup_backend.domain.review.service.ReviewService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -22,7 +22,7 @@ public class ReviewController {
     private final ReviewService reviewService;
 
     @PostMapping("/products/{productId}/reviews")
-    public ApiResponse<ReviewResponseDto> reviewSave(@RequestBody ReviewRequestDto dto, @RequestParam Long productId) {
+    public ApiResponse<ReviewResponseDto> reviewSave(@RequestBody ReviewRequestDto dto, @PathVariable Long productId) {
 
         Long userId = 1L; // 임시 사용자 ID 값 - 추후 JWT 토큰값에서 ID값 가져오는 것으로 변경
         ReviewResponseDto result = reviewService.reviewSave(dto, userId, productId);
