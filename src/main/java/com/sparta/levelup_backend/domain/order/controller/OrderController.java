f4bd497f34b6fc76d13adad17629314b7baaf922
ehdrjs4802 <ehdrjs4802@gmail.com>
2025-02-11 16:44:11 +0900

    init : test push

--- src/main/java/com/sparta/levelup_backend/domain/order/controller/OrderController.java
+++ src/main/java/com/sparta/levelup_backend/domain/order/controller/OrderController.java
@@ -1,4 +1,20 @@
 package com.sparta.levelup_backend.domain.order.controller;
 
+import com.sparta.levelup_backend.common.ApiResponse;
+import com.sparta.levelup_backend.domain.order.service.OrderServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/orders")
+@RequiredArgsConstructor
 public class OrderController {
+
+    private final OrderServiceImpl orderService;
+
+    @PostMapping
+    public ResponseEntity<ApiResponse<OrderCreateDto>> orderCreate
 }
