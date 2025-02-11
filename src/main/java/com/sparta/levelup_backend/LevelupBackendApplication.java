4120547c411a66519cad3586bcfa71f203a8f05d
rlagywnd4 <rlagywnd4@gmail.com>
2025-02-11 12:09:59 +0900

    init: project init

---
+++ src/main/java/com/sparta/levelup_backend/LevelupBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.levelup_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LevelupBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LevelupBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/levelup_backend/LevelupBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.levelup_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LevelupBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
