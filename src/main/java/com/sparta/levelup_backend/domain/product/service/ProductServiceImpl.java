e8fe707fd1df03382d8f27dcdf889cce0d6d46c4
kyung412820 <dlrudgns412@gmail.com>
2025-02-12 14:00:55 +0900

    feat: create product CRUD

--- src/main/java/com/sparta/levelup_backend/domain/product/service/ProductServiceImpl.java
+++ src/main/java/com/sparta/levelup_backend/domain/product/service/ProductServiceImpl.java
@@ -68,8 +68,6 @@ public class ProductServiceImpl implements ProductService{
         ProductEntity product = productRepository.findById(id)
             .orElseThrow(() -> new NotFoundException(ErrorCode.PRODUCT_NOT_FOUND, "상품 ID: " + id));
         product.update(requestDto);
-        product.update(requestDto);
-        product.update(requestDto);
         productRepository.save(product);
         return new ProductUpdateResponseDto(product);
     }
