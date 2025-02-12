dcdd0917ed746294ec5d5534dd5c727590bbb4f4
ehdrjs4802 <ehdrjs4802@gmail.com>
2025-02-12 17:01:19 +0900

    fix : orderRead

--- src/main/java/com/sparta/levelup_backend/domain/order/service/OrderServiceImpl.java
+++ src/main/java/com/sparta/levelup_backend/domain/order/service/OrderServiceImpl.java
@@ -8,6 +8,8 @@ import com.sparta.levelup_backend.domain.product.entity.ProductEntity;
 import com.sparta.levelup_backend.domain.product.service.ProductServiceImpl;
 import com.sparta.levelup_backend.domain.user.entity.UserEntity;
 import com.sparta.levelup_backend.domain.user.userservice.UserServiceImpl;
+import com.sparta.levelup_backend.exception.common.ErrorCode;
+import com.sparta.levelup_backend.exception.common.ForbiddenException;
 import com.sparta.levelup_backend.utill.OrderStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -57,7 +59,14 @@ public class OrderServiceImpl implements OrderService {
      */
     @Override
     public OrderResponseDto findOrder(Long orderId) {
+        Long userId = 2L;
+
         OrderEntity order = orderRepository.findByIdOrElseThrow(orderId);
+
+        // 구매자와 판매자만 조회 가능
+        if (!order.getUser().getId().equals(userId) && !order.getProduct().getUser().getId().equals(userId)) {
+            throw new ForbiddenException(ErrorCode.FORBIDDEN_ACCESS);
+        }
 
         return new OrderResponseDto(order);
     }
