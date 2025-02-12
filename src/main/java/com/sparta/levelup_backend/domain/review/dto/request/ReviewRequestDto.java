741d17f876d25bd019a1396c8c2b666bd844b367
DeaHyun0911 <eogus0911@gmail.com>
2025-02-12 10:59:57 +0900

    feat: review validation
    - Entity 컬럼 제약 조건 추가
    - Review 내용, 별점에 대한 유효성 검사 추가

--- src/main/java/com/sparta/levelup_backend/domain/review/dto/request/ReviewRequestDto.java
+++ src/main/java/com/sparta/levelup_backend/domain/review/dto/request/ReviewRequestDto.java
@@ -1,13 +1,22 @@
 package com.sparta.levelup_backend.domain.review.dto.request;
 
+import static com.sparta.levelup_backend.domain.review.dto.ReviewValidMessage.*;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.validator.constraints.Range;
 
 @Getter
 @RequiredArgsConstructor
 public class ReviewRequestDto {
 
+    @Size(max = 100, message = INVALID_REVIEW_CONTENTS)
     private final String contents;
+
+    @NotNull(message = STAR_SCORE_REQUIRED)
+    @Range(min = 1, max = 5, message = STAR_SCORE_RANGE)
     private final Integer starScore;
 
 }
