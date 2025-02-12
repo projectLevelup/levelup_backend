d4f6f804a5be38b5d5df78d29d129d968c51b44e
DeaHyun0911 <eogus0911@gmail.com>
2025-02-12 20:13:56 +0900

    test: review delete - product, review mismatch
    - 상품, 리뷰 매칭 안될시 예외처리 테스트

--- src/test/java/com/sparta/levelup_backend/domain/review/service/ReviewServiceImplTest.java
+++ src/test/java/com/sparta/levelup_backend/domain/review/service/ReviewServiceImplTest.java
@@ -3,6 +3,7 @@ package com.sparta.levelup_backend.domain.review.service;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.when;
 
+import com.sparta.levelup_backend.domain.product.entity.ProductEntity;
 import com.sparta.levelup_backend.domain.product.repository.ProductRepository;
 import com.sparta.levelup_backend.domain.review.entity.ReviewEntity;
 import com.sparta.levelup_backend.domain.review.repository.ReviewRepository;
@@ -58,6 +59,44 @@ class ReviewServiceImplTest {
             }).isInstanceOf(BusinessException.class)
             .hasMessageContaining(ErrorCode.FORBIDDEN_ACCESS.getMessage());
 
+    }
+
+    @Test
+    void 리뷰와_상품의_매칭_확인() {
+        //given
+        Long userId = 1L;
+        Long productId = 1L;
+        Long otherProductId = 2L;
+        Long reviewId = 1L;
+
+        // 관리자 계정
+        UserEntity user = UserEntity.builder()
+            .id(userId)
+            .role(UserRole.ADMIN)
+            .build();
+
+        ProductEntity product1 = ProductEntity.builder()
+            .id(productId)
+            .build();
+
+        ProductEntity product2 = ProductEntity.builder()
+            .id(otherProductId)
+            .build();
+
+        ReviewEntity review = ReviewEntity.builder()
+            .id(reviewId)
+            .product(product2)
+            .build();
+
+        //when
+        when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+        when(reviewRepository.findById(reviewId)).thenReturn(Optional.of(review));
+
+        //then
+        assertThatThrownBy(() -> {
+            reviewService.reviewDelete(userId, productId, reviewId);
+        }).isInstanceOf(BusinessException.class)
+            .hasMessageContaining(ErrorCode.MISMATCH_REVIEW_PRODUCT.getMessage());
     }
 
 
