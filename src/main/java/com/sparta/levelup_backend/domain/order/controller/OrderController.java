14b4671f8985aa61bca7a5ec10f86342c22ea16c
ehdrjs4802 <ehdrjs4802@gmail.com>
2025-02-12 16:32:10 +0900

    move : develop pull move

--- src/main/java/com/sparta/levelup_backend/domain/order/controller/OrderController.java
+++ src/main/java/com/sparta/levelup_backend/domain/order/controller/OrderController.java
@@ -9,14 +9,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import static com.sparta.levelup_backend.common.ApiResMessage.*;
 import static com.sparta.levelup_backend.common.ApiResponse.*;
 import static org.springframework.http.HttpStatus.*;
-import static com.sparta.levelup_backend.common.ApiResponse.*;
+
 
 @RestController
 @RequestMapping("/v1/orders")
@@ -27,23 +25,19 @@ public class OrderController {
 
     // 주문 생성
     @PostMapping
-    public ApiResponse<OrderCreateResponseDto> orderCreate(
+    public ApiResponse<OrderResponseDto> orderCreate(
             @RequestBody OrderCreateRequestDto dto
     ) {
-        OrderCreateResponseDto orderCreateResponseDto = orderService.orderCreate(dto);
+        OrderResponseDto orderCreateResponseDto = orderService.orderCreate(dto);
         return success(OK, ORDER_CREATE, orderCreateResponseDto);
     }
 
     // 주문 조회
     @GetMapping("/{orderId}")
-    public ResponseEntity<ApiResponse<OrderResponseDto>> findOrder(
+    public ApiResponse<OrderResponseDto> findOrder(
             @PathVariable Long orderId
     ) {
         OrderResponseDto orderById = orderService.findOrder(orderId);
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(success(
-                        ORDER_FIND,
-                        orderById
-                ));
+        return success(OK, ORDER_FIND, orderById);
     }
 }
