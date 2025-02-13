ce8d44fd3eee620a090c5812784ae58703ca3567
DeaHyun0911 <eogus0911@gmail.com>
2025-02-13 21:30:14 +0900

    fix: reviews find nickname / productId remove
    - 리뷰 목록 조회에 닉네임 추가
    - 리뷰 목록 조회에 productId 제거

--- src/main/java/com/sparta/levelup_backend/domain/review/dto/response/ReviewResponseDto.java
+++ src/main/java/com/sparta/levelup_backend/domain/review/dto/response/ReviewResponseDto.java
@@ -2,34 +2,34 @@ package com.sparta.levelup_backend.domain.review.dto.response;
 
 import com.querydsl.core.annotations.QueryProjection;
 import com.sparta.levelup_backend.domain.review.entity.ReviewEntity;
-import lombok.AllArgsConstructor;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
 public class ReviewResponseDto {
 
-    private Long reviewId;
-    private Long productId;
-    private Long userId;
-    private String contents;
-    private Integer starScore;
+	private Long reviewId;
+	private Long userId;
+	private String nickname;
+	private String contents;
+	private Integer starScore;
 
-    public ReviewResponseDto(ReviewEntity review) {
-        this.reviewId = review.getId();
-        this.productId = review.getProduct().getId();
-        this.userId = review.getUser().getId();
-        this.contents = review.getContents();
-        this.starScore = review.getStarScore();
-    }
+	public ReviewResponseDto(ReviewEntity review) {
+		this.reviewId = review.getId();
+		this.userId = review.getUser().getId();
+		this.nickname = review.getUser().getNickName();
+		this.contents = review.getContents();
+		this.starScore = review.getStarScore();
+	}
 
-    @QueryProjection
-    public ReviewResponseDto(Long reviewId, Long userId, String contents, Integer starScore) {
-        this.reviewId = reviewId;
-        this.userId = userId;
-        this.contents = contents;
-        this.starScore = starScore;
-    }
+	@QueryProjection
+	public ReviewResponseDto(Long reviewId, Long userId, String nickname, String contents, Integer starScore) {
+		this.reviewId = reviewId;
+		this.userId = userId;
+		this.nickname = nickname;
+		this.contents = contents;
+		this.starScore = starScore;
+	}
 }
