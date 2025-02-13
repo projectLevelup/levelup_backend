eca8070701df66d365dc6630320bf0470e5b8cae
DeaHyun0911 <eogus0911@gmail.com>
2025-02-13 10:49:57 +0900

    feat: Check Order Completed
    - 거래 완료한 사용자인지 검사하는 로직 추가

--- src/main/java/com/sparta/levelup_backend/domain/review/service/ReviewServiceImpl.java
+++ src/main/java/com/sparta/levelup_backend/domain/review/service/ReviewServiceImpl.java
@@ -1,5 +1,9 @@
 package com.sparta.levelup_backend.domain.review.service;
 
+import static com.sparta.levelup_backend.exception.common.ErrorCode.*;
+import static com.sparta.levelup_backend.utill.OrderStatus.*;
+
+import com.sparta.levelup_backend.domain.order.repository.OrderRepository;
 import com.sparta.levelup_backend.domain.product.entity.ProductEntity;
 import com.sparta.levelup_backend.domain.product.repository.ProductRepository;
 import com.sparta.levelup_backend.domain.review.dto.request.ReviewRequestDto;
@@ -9,9 +13,11 @@ import com.sparta.levelup_backend.domain.review.repository.ReviewQueryRepository
 import com.sparta.levelup_backend.domain.review.repository.ReviewRepository;
 import com.sparta.levelup_backend.domain.user.entity.UserEntity;
 import com.sparta.levelup_backend.domain.user.repository.UserRepository;
+import com.sparta.levelup_backend.exception.common.BusinessException;
 import com.sparta.levelup_backend.exception.common.ErrorCode;
 import com.sparta.levelup_backend.exception.common.ForbiddenAccessException;
 import com.sparta.levelup_backend.exception.common.MismatchException;
+import com.sparta.levelup_backend.utill.OrderStatus;
 import com.sparta.levelup_backend.utill.UserRole;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
@@ -27,6 +33,7 @@ public class ReviewServiceImpl implements ReviewService {
     private final ReviewQueryRepository reviewQueryRepository;
     private final UserRepository userRepository;
     private final ProductRepository productRepository;
+    private final OrderRepository orderRepository;
 
     @Override
     @Transactional
@@ -34,6 +41,11 @@ public class ReviewServiceImpl implements ReviewService {
 
         UserEntity user = userRepository.findById(userId).orElseThrow(RuntimeException::new); // Todo: 변경 예정
         ProductEntity product = productRepository.findById(productId).orElseThrow(RuntimeException::new); // Todo: 변경 예정
+
+        // 해당 상품을 거래 완료한 사용자인지 확인
+        if (!orderRepository.existsByUserIdAndProductIdAndStatus(userId, productId, COMPLETED)) {
+            throw new ForbiddenAccessException(COMPLETED_ORDER_REQUIRED);
+        }
 
         ReviewEntity review = ReviewEntity.builder()
             .contents(dto.getContents())
@@ -55,14 +67,14 @@ public class ReviewServiceImpl implements ReviewService {
 
         // 리뷰 삭제는 관리자 권한만 실행 가능
         if(!user.getRole().equals(UserRole.ADMIN)) {
-            throw new ForbiddenAccessException(ErrorCode.FORBIDDEN_ACCESS);
+            throw new ForbiddenAccessException(FORBIDDEN_ACCESS);
         }
 
         ReviewEntity review = reviewRepository.findByIdOrElseThrow(reviewId);
 
         //리뷰가 해당 상품의 리뷰인 지 확인
         if(!review.getProduct().getId().equals(productId)) {
-            throw new MismatchException(ErrorCode.MISMATCH_REVIEW_PRODUCT);
+            throw new MismatchException(MISMATCH_REVIEW_PRODUCT);
         }
 
         review.deleteReview();
--- src/main/java/com/sparta/levelup_backend/exception/common/ErrorCode.java
+++ src/main/java/com/sparta/levelup_backend/exception/common/ErrorCode.java
@@ -15,6 +15,7 @@ public enum ErrorCode {
 
     // 403 FORBIDDEN
     FORBIDDEN_ACCESS(HttpStatus.FORBIDDEN, "ERR201", "접근 권한이 없습니다."),
+    COMPLETED_ORDER_REQUIRED(HttpStatus.FORBIDDEN, "ERR202", "해당 상품을 거래 완료한 사용자만 리뷰를 작성할 수 있습니다."),
 
     // 404 NOT_FOUND
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "ERR301", "사용자를 찾을 수 없습니다."),
