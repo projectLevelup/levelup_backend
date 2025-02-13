540e5f85cd1dfa8eeea06db266931372befa1dbb
DeaHyun0911 <eogus0911@gmail.com>
2025-02-13 11:01:53 +0900

    test: Exist Completed Order
    - 거래 완료된 주문이 없으면 예외발생 테스트

--- src/test/java/com/sparta/levelup_backend/domain/review/service/ReviewServiceImplTest.java
+++ src/test/java/com/sparta/levelup_backend/domain/review/service/ReviewServiceImplTest.java
@@ -4,14 +4,19 @@ import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.when;
 
+import com.sparta.levelup_backend.domain.order.entity.OrderEntity;
+import com.sparta.levelup_backend.domain.order.repository.OrderRepository;
 import com.sparta.levelup_backend.domain.product.entity.ProductEntity;
 import com.sparta.levelup_backend.domain.product.repository.ProductRepository;
+import com.sparta.levelup_backend.domain.review.dto.request.ReviewRequestDto;
 import com.sparta.levelup_backend.domain.review.entity.ReviewEntity;
 import com.sparta.levelup_backend.domain.review.repository.ReviewRepository;
 import com.sparta.levelup_backend.domain.user.entity.UserEntity;
 import com.sparta.levelup_backend.domain.user.repository.UserRepository;
 import com.sparta.levelup_backend.exception.common.BusinessException;
 import com.sparta.levelup_backend.exception.common.ErrorCode;
+import com.sparta.levelup_backend.exception.common.ForbiddenAccessException;
+import com.sparta.levelup_backend.utill.OrderStatus;
 import com.sparta.levelup_backend.utill.UserRole;
 import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
@@ -37,6 +42,9 @@ class ReviewServiceImplTest {
     @Mock
     private ProductRepository productRepository;
 
+    @Mock
+    private OrderRepository orderRepository;
+
     // 테스트별 공통으로 사용할 데이터
     private Long userId = 1L;
     private Long productId = 1L;
@@ -45,6 +53,7 @@ class ReviewServiceImplTest {
     private UserEntity adminUser;
     private ProductEntity product;
     private ReviewEntity review;
+    private OrderEntity completeOrder;
 
     @BeforeEach
     void setUp() {
@@ -65,6 +74,12 @@ class ReviewServiceImplTest {
         review = ReviewEntity.builder()
             .id(reviewId)
             .product(product)
+            .build();
+
+        completeOrder = OrderEntity.builder()
+            .user(normalUser)
+            .product(product)
+            .status(OrderStatus.COMPLETED)
             .build();
     }
 
@@ -114,9 +129,22 @@ class ReviewServiceImplTest {
         when(reviewRepository.findById(reviewId)).thenReturn(Optional.of(review));
         reviewService.deleteReview(userId, productId, reviewId);
 
-        // then
+        //then
         assertThat(review.getIsDeleted()).isTrue();
     }
 
+    @Test
+    void 거래_완료된_주문이_없을때_예외발생() {
+        //when
+        when(userRepository.findById(userId)).thenReturn(Optional.of(normalUser));
+        when(productRepository.findById(productId)).thenReturn(Optional.of(product));
+        when(orderRepository.existsByUserIdAndProductIdAndStatus(userId, productId, OrderStatus.COMPLETED)).thenReturn(false);
+
+        //then
+        assertThatThrownBy(() -> {
+            reviewService.saveReview(new ReviewRequestDto("리뷰 테스트", 5), userId, productId);
+        }).isInstanceOf(ForbiddenAccessException.class)
+            .hasMessageContaining(ErrorCode.COMPLETED_ORDER_REQUIRED.getMessage());
+    }
 
 }
