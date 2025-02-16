72ee8e5ea3a4516e5a5b88cb8f26d3dabde253d5
lq0920084 <lq0920084@gmail.com>
2025-02-16 15:02:31 +0900

    Feat: SignUp Json to Form AND add FormToJson Converter

---
+++ src/main/java/com/sparta/levelup_backend/config/FormToJsonHttpMessageConverter.java
@@ -0,0 +1,38 @@
+package com.sparta.levelup_backend.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sparta.levelup_backend.config.annotaion.FormToJson;
+import java.io.IOException;
+import org.springframework.http.HttpInputMessage;
+import org.springframework.http.HttpOutputMessage;
+import org.springframework.http.converter.AbstractHttpMessageConverter;
+import org.springframework.http.converter.FormHttpMessageConverter;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.converter.HttpMessageNotWritableException;
+import org.springframework.util.MultiValueMap;
+
+public class FormToJsonHttpMessageConverter<T> extends AbstractHttpMessageConverter<T> {
+
+    @Override
+    protected boolean supports(Class<?> clazz) {
+        return clazz.isAnnotationPresent(FormToJson.class);
+    }
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    private static final FormHttpMessageConverter converter = new FormHttpMessageConverter();
+
+    @Override
+    protected T readInternal(Class<? extends T> clazz, HttpInputMessage inputMessage)
+        throws IOException, HttpMessageNotReadableException {
+        MultiValueMap<String, String> read = converter.read(null, inputMessage);
+        return objectMapper.convertValue(read.asSingleValueMap(), clazz);
+    }
+
+    @Override
+    protected void writeInternal(T t, HttpOutputMessage outputMessage)
+        throws IOException, HttpMessageNotWritableException {
+
+    }
+
+}
--- src/main/java/com/sparta/levelup_backend/domain/auth/controller/AuthController.java
+++ src/main/java/com/sparta/levelup_backend/domain/auth/controller/AuthController.java
@@ -1,26 +1,31 @@
 package com.sparta.levelup_backend.domain.auth.controller;
 
-import static com.sparta.levelup_backend.common.ApiResMessage.*;
-import static com.sparta.levelup_backend.common.ApiResponse.*;
-import static org.springframework.http.HttpStatus.*;
-
-import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import static com.sparta.levelup_backend.common.ApiResMessage.SIGNUP_SUCCESS;
+import static com.sparta.levelup_backend.common.ApiResponse.success;
+import static org.springframework.http.HttpStatus.CREATED;
 
 import com.sparta.levelup_backend.common.ApiResponse;
 import com.sparta.levelup_backend.domain.auth.dto.request.SignUpUserRequestDto;
 import com.sparta.levelup_backend.domain.auth.service.AuthService;
-
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
+@Controller
 @RequestMapping("/v1")
 @RequiredArgsConstructor
 public class AuthController {
 	private final AuthService authService;
+
+	@GetMapping("/signup")
+	public String signUpUserPage(){
+
+		return "signup";
+	}
 
 	@PostMapping("/signup")
 	public ApiResponse<Void> signUpUser(@Valid @RequestBody SignUpUserRequestDto dto) {
