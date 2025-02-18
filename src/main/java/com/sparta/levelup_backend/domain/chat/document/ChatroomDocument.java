8e42093c31ea9d0e636f34048f111469cbc6fff7
DeaHyun0911 <eogus0911@gmail.com>
2025-02-18 16:41:39 +0900

    feat: chatroomDocument
    - 채팅방 Document 추가
    - 제목, 참여자, 마지막 메시지, 안읽은 메시지 필드 추가

---
+++ src/main/java/com/sparta/levelup_backend/domain/chat/document/ChatroomDocument.java
@@ -0,0 +1,42 @@
+package com.sparta.levelup_backend.domain.chat.document;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+@Document(collection = "chatroom")
+public class ChatroomDocument {
+
+	@Id
+	private String id;
+	private String title;
+	private List<Participant> participants;
+	private String lastMessage;
+	private Map<String, Integer> unreadMessages;
+	private boolean isDeleted;
+
+	@CreatedDate
+	private LocalDateTime createdAt;
+
+	@LastModifiedDate
+	private LocalDateTime updatedAt;
+
+	public void updateDeleted() {
+		isDeleted = true;
+	}
+
+	public void updateParticipants(List<Participant> participants) {
+		this.participants = participants;
+	}
+
+}
