d3842a643a65eb5474da98080b2b4f6cba75da7b
DeaHyun0911 <eogus0911@gmail.com>
2025-02-19 13:18:07 +0900

    test: create chat success
    - 채팅방 생성 성공 테스트

---
+++ src/test/java/com/sparta/levelup_backend/domain/chat/service/ChatroomServiceImplTest.java
@@ -0,0 +1,85 @@
+package com.sparta.levelup_backend.domain.chat.service;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.sparta.levelup_backend.domain.chat.document.ChatroomDocument;
+import com.sparta.levelup_backend.domain.chat.document.Participant;
+import com.sparta.levelup_backend.domain.chat.dto.ChatroomCreateResponseDto;
+import com.sparta.levelup_backend.domain.chat.repository.ChatroomMongoRepository;
+import com.sparta.levelup_backend.domain.user.entity.UserEntity;
+import com.sparta.levelup_backend.domain.user.repository.UserRepository;
+
+@ExtendWith(MockitoExtension.class)
+class ChatroomServiceImplTest {
+
+	@InjectMocks
+	private ChatroomServiceImpl chatroomService;
+
+	@Mock
+	private ChatroomMongoRepository chatroomMongoRepository;
+
+	@Mock
+	private UserRepository userRepository;
+
+	private final UserEntity publisher = UserEntity.builder()
+		.id(1L)
+		.nickName("Publisher")
+		.build();
+
+	private final UserEntity subscriber = UserEntity.builder()
+		.id(2L)
+		.nickName("Subscriber")
+		.build();
+
+	private final Participant participant1 = new Participant(publisher);
+	private final Participant participant2 = new Participant(subscriber);
+
+	@Test
+	void 채팅방_생성_성공() {
+		//given
+		Long userId = 1L;
+		Long targetUserId = 2L;
+		String title = "title";
+
+		ChatroomDocument chatroomDocument = ChatroomDocument.builder()
+			.id("test")
+			.title(title)
+			.participants(Arrays.asList(participant1, participant2))
+			.lastMessage("")
+			.unreadMessages(new HashMap<>())
+			.build();
+
+		when(chatroomMongoRepository.countByParticipantsUserIds(Arrays.asList(targetUserId, userId))).thenReturn(0L);
+		when(userRepository.findByIdOrElseThrow(userId)).thenReturn(publisher);
+		when(userRepository.findByIdOrElseThrow(targetUserId)).thenReturn(subscriber);
+		when(chatroomMongoRepository.save(any(ChatroomDocument.class))).thenReturn(chatroomDocument);
+
+		//when
+		ChatroomCreateResponseDto actualResult = chatroomService.createChatroom(userId, targetUserId, title);
+
+		//then
+		ChatroomCreateResponseDto expectedResult = ChatroomCreateResponseDto.builder()
+			.chatroomId("test")
+			.title(title)
+			.participants(Arrays.asList(participant1, participant2))
+			.build();
+
+		assertThat(actualResult)
+			.usingRecursiveComparison()
+			.isEqualTo(expectedResult);
+		verify(chatroomMongoRepository, times(1)).save(any(ChatroomDocument.class));
+
+	}
+
+
+}
