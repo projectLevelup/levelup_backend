1d88d6c668e3a98a0271922056c01a5dfeaa433b
DeaHyun0911 <eogus0911@gmail.com>
2025-02-23 22:46:10 +0900

    test: application-test.yml

--- src/test/java/com/sparta/levelup_backend/LevelupBackendApplicationTests.java
+++ src/test/java/com/sparta/levelup_backend/LevelupBackendApplicationTests.java
@@ -1,15 +1,8 @@
 package com.sparta.levelup_backend;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
 class LevelupBackendApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
