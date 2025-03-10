ba168224c7b45964c00553ff0cd341643ad1b38b
DeaHyun0911 <eogus0911@gmail.com>
2025-03-10 11:28:54 +0900

    recfactor: websocket thread pool custom
    - websocket 연결시 쓰레드풀 커스텀설정 최적화

--- src/main/java/com/sparta/levelup_backend/config/StompConfig.java
+++ src/main/java/com/sparta/levelup_backend/config/StompConfig.java
@@ -1,6 +1,7 @@
 package com.sparta.levelup_backend.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -18,6 +19,24 @@ public class StompConfig implements WebSocketMessageBrokerConfigurer {
 		registry.addEndpoint("/ws/chats")
 			.setAllowedOriginPatterns("*")
 			.withSockJS();
+	}
+
+	@Override
+	public void configureClientInboundChannel(ChannelRegistration registration) {
+		registration.taskExecutor()
+			.corePoolSize(10)
+			.maxPoolSize(20)
+			.keepAliveSeconds(30)
+			.queueCapacity(100);
+	}
+
+	@Override
+	public void configureClientOutboundChannel(ChannelRegistration registration) {
+		registration.taskExecutor()
+			.corePoolSize(10)
+			.maxPoolSize(20)
+			.keepAliveSeconds(30)
+			.queueCapacity(100);
 	}
 
 	/**
