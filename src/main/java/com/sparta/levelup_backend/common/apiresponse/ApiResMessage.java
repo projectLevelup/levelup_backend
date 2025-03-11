9c965173be6463586139687b0292cbc6bfdc0e60
rlagywnd4 <rlagywnd4@gmail.com>
2025-03-11 13:56:22 +0900

    fix: change ApiResMessage
    반환 메세지 통일

--- src/main/java/com/sparta/levelup_backend/common/apiresponse/ApiResMessage.java
+++ src/main/java/com/sparta/levelup_backend/common/apiresponse/ApiResMessage.java
@@ -32,10 +32,10 @@ public class ApiResMessage {
     public static final String PRODUCT_DELETE = "상품이 삭제되었습니다";
 
     // 게임 관련 성공 메시지
-    public static final String GAME_SAVE_SUCCESS = "게임 등록이 성공적으로 완료되었습니다.";
-    public static final String GAME_FOUND_SUCCESS = "게임을 성공적으로 조회되었습니다.";
-    public static final String GAME_UPDATE_SUCCESS = "게임을 성공적으로 수정되었습니다.";
-    public static final String GAME_DELETE_SUCCESS = "게임이 성공적으로 삭제되었습니다.";
+    public static final String GAME_SAVE_SUCCESS = "게임이 생성되었습니다.";
+    public static final String GAME_FOUND_SUCCESS = "게임이 조회되었습니다.";
+    public static final String GAME_UPDATE_SUCCESS = "게임이 수정되었습니다.";
+    public static final String GAME_DELETE_SUCCESS = "게임이 삭제되었습니다.";
 
     // Chat
     public static final String CHATROOM_CREATE = "채팅방이 생성되었습니다";
